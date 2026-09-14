1df4fbac1b8ae65d2759d51e78bf5b831245abb5
Aytan-sudo <cantais.aymeric@gmail.com>
2026-09-14 16:40:58 +0200

    fix: conserver les caches des autres jeux

--- sw.js
+++ sw.js
@@ -49,7 +49,7 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys()
             .then(keys => Promise.all(
-                keys.filter(key => key !== VERSION).map(key => caches.delete(key))
+                keys.filter(key => key.startsWith('sutom-') && key !== VERSION).map(key => caches.delete(key))
             ))
             .then(() => self.clients.claim())
     );
